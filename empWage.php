56ef1dd3fa341fb560dcd4496d3d21c887daf18e
sheetal <sheetalbedarkar96@gmail.com>
2021-04-17 17:33:02 +0530

    Added: UC5 calculate wage for month

--- empWage.php
+++ empWage.php
@@ -1,17 +1,22 @@
 <?php
-    $empCheck = (rand(0, 2));
     $empRatePerHr = 20;
+    $numWorkingDays=20;
+    $totalSalary=0;
 
-    switch ($empCheck) {
-        case 1:
-            $empHrs = 8;
-            break;
-        case 2:
-            $empHrs = 4;
-            break;
-        default:
-            $empHrs = 0;
-    }
+    for ($day = 1; $day <= 20; $day++) {
+        $empCheck = (rand(0, 2));
+        switch ($empCheck) {
+            case 1:
+                $empHrs = 8;
+                break;
+            case 2:
+                $empHrs = 4;
+                break;
+            default:
+                $empHrs = 0;
+        }
     $salary = $empRatePerHr * $empHrs;
-    echo $salary;
+    $totalSalary = $totalSalary + $salary;
+    }
+    echo "Total Salary is : $totalSalary";
 ?>
