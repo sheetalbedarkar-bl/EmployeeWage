4a1c5cbbc2abc80556fd0c38dd158a41160cdfe5
sheetal <sheetalbedarkar96@gmail.com>
2021-04-17 17:16:15 +0530

    Added: UC4 using switch case

--- empWage.php
+++ empWage.php
@@ -1,14 +1,16 @@
 <?php
     $empCheck = (rand(0, 2));
     $empRatePerHr = 20;
-    if ($empCheck == 1) {
-        $empHrs = 8;
-    }
-    elseif ($empCheck == 2) {
-        $empHrs = 4;
-    }
-    else {
-        $empHrs = 0;
+
+    switch ($empCheck) {
+        case 1:
+            $empHrs = 8;
+            break;
+        case 2:
+            $empHrs = 4;
+            break;
+        default:
+            $empHrs = 0;
     }
     $salary = $empRatePerHr * $empHrs;
     echo $salary;
